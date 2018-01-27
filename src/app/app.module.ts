08fcf9bf8747a58a5f7e6a33ba36933274903979
snj <shubham.joshi1507@gmail.com>
2018-01-26 20:12:25 -0800

    Ionic 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,31 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {HttpClientModule} from '@angular/common/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {ArchivedTodosPage} from "../pages/archived-todos/archived-todos";
+import { TodoService } from '../providers/todo-service/todo-service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,ArchivedTodosPage
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,ArchivedTodosPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TodoService
   ]
 })
 export class AppModule {}
